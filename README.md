8addfc386e327e469ed37bbfc6809820dd8000ff
Hung-I Wang <whygowe@gmail.com>
2019-09-07 22:52:56 +0800

    Just works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Group Private Message Bot
+# Low Voice Bot
+## What?
+Low Voice Bot (@lowvoicebot) helps send private messages secretly to someone in public groups via [Inline Mode](https://core.telegram.org/bots/inline).
+
+## Why?
+No more "PM sent". No need to be mutual contacts. 
---
+++ lowvoicebot.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+import os
+from collections import namedtuple
+import re
+from re import Match
+import asyncio
+from time import time as now
+
+from async_lru import alru_cache
+from aiohttp import ClientSession as HTTPClientSession
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle, InlineKeyboardMarkup, \
+    InlineKeyboardButton, CallbackQuery
+from aiogram.utils.exceptions import InvalidQueryID
+from aiogram.types.message import ParseMode
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+types.User.__repr__ = lambda u: f"User({u.mention})"
+
+logger = logging.getLogger('lowvoicebot')
+
+
+class BotTokenUnspecified(Exception):
+    pass
+
+
+class ReadableException(Exception):
+    content = "❌ Unspecified Error ❌"
+
+    def __init__(self, content, *args, **kwargs):
+        self.content = content
+        super().__init__(*args, **kwargs)
+
+
+REGEX_RESOLVE_USER = re.compile(r'<div\ class="tgme_page_title">(.+?)</div>')
+HTTP_SESSION = None
+
+
+@alru_cache(maxsize=1024)
+async def resolve_user(username):
+    global HTTP_SESSION
+    if not HTTP_SESSION:
+        HTTP_SESSION = HTTPClientSession()
+    try:
+        async with HTTP_SESSION.get(f"https://t.me/{username}") as response:
+            assert response.status is 200
+            return REGEX_RESOLVE_USER.search(await response.text()).group(1)
+    except Exception as e:
+        logger.debug(f"Failed to resolve {username} with error {e}")
+        return None
+
+
+WhisperEntry = namedtuple("WhisperEntry", ("sender", "recipient", "content"))
+
+whispers = dict()
+
+expiring_tasks = dict()
+
+
+async def expire_whisper(id, in_seconds=0):
+    await asyncio.sleep(in_seconds)
+    expired_whisper = whispers.pop(id)
+    del expiring_tasks[id]
+    logger.debug(f"Message expired: {expired_whisper!s}")
+
+
+bot_token = os.environ.get("BOT_TOKEN")
+if not bot_token:
+    with open("./.BOT_TOKEN") as f:
+        bot_token = f.read().strip()
+    if not bot_token:
+        raise BotTokenUnspecified
+
+bot = Bot(token=bot_token)
+dispatcher = Dispatcher(bot)
+
+
+# @dispatcher.message_handler(commands=["start"])
+# async def start_handler(message: types.Message):
+#     logger.debug(f"start_handler:{message.text}")
+#     args = message.get_args()
+#     if not args:
+#         await message.reply("Low Voice Bot helps send ㊙️private ✉️messages in public groups."
+#                             "\n[How to start?](https://github.com/Gowee/LowVoiceBot#How?)", parse_mode=ParseMode.MARKDOWN)
+
+@dispatcher.message_handler(CommandStart(re.compile(r"SAVE\_(?P<whisper_id>.+)")))
+async def start_save_handler(message: types.Message, deep_link: re.Match):
+    logger.debug(f"start_save_handler:{deep_link}")
+    try:
+        try:
+            whisper_id = deep_link.group("whisper_id")
+        except IndexError:
+            raise ReadableException("❌ Malformed arguments")
+        whisper = whispers.get(whisper_id)
+        if whisper is None:
+            logger.debug(f"start_handler:Invalid whisper id: {whisper_id}")
+            raise ReadableException(
+                "⏲️🔔/❌ The message is expired or non-existent.")
+        if message.from_user.username != whisper.recipient and message.from_user != whisper.sender:
+            raise ReadableException(
+                "🚫 You are neither the sender nor recipient.")
+        await message.answer(f"_Message from_ {whisper.sender.mention} _to_ @{whisper.recipient}:\n\n{whisper.content}", parse_mode=ParseMode.MARKDOWN)
+    except ReadableException as e:
+        await message.reply(e.content, parse_mode=ParseMode.MARKDOWN)
+
+
+@dispatcher.message_handler(commands=["ping"])
+async def ping_handler(message: types.Message):
+    await message.reply("Pong")
+
+
+@dispatcher.inline_handler()
+async def whisper_inline_handler(query: InlineQuery):
+    logger.debug(f"whisper_inline_handler")
+    try:
+        bot_username = (await bot.get_me()).username
+        sender = query.from_user
+        if not query.query:
+            raise ReadableException(
+                (f"EMPTY_ARG",
+                 f"ℹ️ Show Usage",
+                 f"ℹ️ Usage: `@{bot_username} @RECIPIENT message)`"))
+        try:
+            recipient, whisper = query.query.split(maxsplit=2)
+        except ValueError:
+            raise ReadableException(("USAGE_ERROR",
+                                     "❌ Usage Error",
+                                     f"❌ Usage Error\nUsage: `@{bot_username} @RECIPIENT message`"))
+
+        if recipient.startswith("@"):
+            recipient = recipient[1:]
+        recipient_name = await resolve_user(recipient)
+        if recipient_name is None:
+            raise ReadableException(
+                ("INVALID_USERNAME", "❌ Invalid Username", "❌ Invalid Username"))
+        whisper_id = f"WHISPER-{int(now()):x}-{id(whisper):x}"
+        input_content = InputTextMessageContent(
+            f"*㊙️Private Message✉️*\n_To_ {recipient_name}(@{recipient}),\nexpiring in 30 minutes.", parse_mode=ParseMode.MARKDOWN)
+        button_reveal = InlineKeyboardButton(
+            "🔎 Reveal", callback_data=f"REVEAL|{whisper_id}")
+        button_save = InlineKeyboardButton(
+            "💾 Save", url=f"https://t.me/{bot_username}?start=SAVE_{whisper_id}")
+        button_expire = InlineKeyboardButton(
+            "🛑 Expire", callback_data=f"EXPIRE|{whisper_id}")
+        item_default = InlineQueryResultArticle(
+            id=f"{whisper_id}-1",
+            # f'✉️ To {recipient_name}, ⏲️ Expire in 30 minutes',
+            title=f'With Save button',
+            input_message_content=input_content,
+            reply_markup=InlineKeyboardMarkup(
+                1, [[button_reveal, button_save, button_expire]]),
+        )
+        item_nosave = InlineQueryResultArticle(
+            id=f"{whisper_id}-2",
+            # f'✉️ To {recipient_name}, \n⏲️ Expire in 30 minutes, No save button',
+            title=f"Without Save button",
+            input_message_content=input_content,
+            reply_markup=InlineKeyboardMarkup(
+                1, [[button_reveal, button_expire]]),
+        )
+        try:
+            if (await query.answer(results=[item_default, item_nosave], cache_time=3, is_personal=True)) is True:
+                logger.info(
+                    f"From {sender.mention} to {recipient_name}(@{recipient}): WHISPER_REDACTED")
+                whispers[whisper_id] = WhisperEntry(sender, recipient, whisper)
+                # TODO: permenant trhu  encryption
+                expiring_tasks[whisper_id] = asyncio.create_task(
+                    expire_whisper(whisper_id, 1800))
+        except InvalidQueryID as e:
+            logger.debug(f"{e} query: {query}, message: {whisper}")
+    except ReadableException as e:
+        logger.debug(f"whisper_inline_handler:ReadableException({e.content})")
+        input_content = InputTextMessageContent(
+            e.content[2],
+            ParseMode.MARKDOWN
+        )
+        item = InlineQueryResultArticle(
+            id=e.content[0],
+            title=e.content[1],
+            input_message_content=input_content
+        )
+        try:
+            await query.answer(results=[item], cache_time=3600)
+        except InvalidQueryID as e:
+            logger.debug(f"{e}(inside ReadableException) {query}")
+
+
+@dispatcher.callback_query_handler()
+async def whisper_reveal_handler(query: CallbackQuery):
+    logger.debug(f"whisper_reveal_handler")
+
+    def answer_error(error_text):
+        return query.answer(error_text, cache_time=1800)
+    try:
+        action, whisper_id = query.data.split("|", maxsplit=2)
+        whisper = whispers.get(whisper_id)
+        if whisper is None:
+            logger.debug(
+                f"whisper_reveal_handler:Invalid whisper id, data: {query.data}")
+            raise ReadableException(
+                "⏲️🔔/❌ The message is expired or non-existent.")
+        if query.from_user.username != whisper.recipient and query.from_user != whisper.sender:
+            raise ReadableException(
+                "🚫 You are neither the sender nor recipient.")
+        if action == "REVEAL":
+            await query.answer(f"From {whisper.sender.mention} to @{whisper.recipient}:\n\n{whisper.content}",
+                               cache_time=30, show_alert=True)
+        elif action == "EXPIRE":
+            await expire_whisper(whisper_id)
+            await query.answer(f"✅ Successfully expired.", cache_time=1800)
+        else:
+            raise ReadableException("❌ Unsupported Action ❌")
+    except ReadableException as e:
+        await answer_error(e.content)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    #logger.setLevel(logging.DEBUG)
+
+    executor.start_polling(dispatcher)
+
+
+if __name__ == "__main__":
+    main()
